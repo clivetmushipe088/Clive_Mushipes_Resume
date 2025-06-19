4fb3f25b1d6d92c408d4724c822a535293c33439
clivetmushipe088 <c.mushipe@alustudent.com>
2025-06-19 18:11:04 +0200

    added

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("toggleMode");
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+});
